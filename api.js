50e4fe3c45fef1eb654ff41e9c4a191a2e90da66
Daniel Kütt <danielkytt@hotmail.com>
2019-07-14 00:21:25 +0300

    (Should be dev branch) Made a function for getting data for a day

--- api.js
+++ api.js
@@ -14,5 +14,22 @@ router.get('/temperatures', (req, res) => {
   })
 });
 
+/*
+database.getDatabaseConnection()
+  .then(conn => {
+    return conn.query('SELECT * FROM temperature ' +
+      'WHERE datetime > (NOW() - INTERVAL 1 DAY) AND location = \'katel\';')
+      .then(rows => {
+        for (let i in rows) {
+          console.log(rows[i], 'END OF ROW')
+        }
+        console.log('END OF DATA');
+      })
+      .catch(err => console.warn(err))
+      .then(() => conn.end());
+  })
+  .catch(err => console.warn(err));
+*/
+
 module.exports = router;
 
