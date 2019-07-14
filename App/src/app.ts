368ba8230a7b27bf270ccdaab60ef79cf7a80af4
Daniel Kütt <danielkytt@hotmail.com>
2019-07-14 12:39:54 +0300

    Added case handler when there is little data for chart

--- App/src/app.ts
+++ App/src/app.ts
@@ -29,7 +29,7 @@ export class App {
 
   drawGraph(data) {
     console.log(data.temperatures);
-    let temperatures = data.temperatures.filter((temp, i) => i % 5 == 4);
+    let temperatures = data.temperatures.length > 10 ? data.temperatures.filter((temp, i) => i % 5 == 4) : data.temperatures;
     this.chartData = {
       type: 'line',
       data: {
