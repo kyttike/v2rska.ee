2620066753d82a0a9b259d945c4e5666263634b1
kyttike <danielkytt@hotmail.com>
2020-12-29 21:26:20 +0200

    Add temperature gradient to line

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -2,6 +2,7 @@ import {HttpClient} from "aurelia-fetch-client";
 import * as moment from "moment";
 import {EventAggregator} from "aurelia-event-aggregator";
 import {inject, observable} from 'aurelia-framework';
+import * as ChartJs from 'chart.js';
 
 @inject(EventAggregator)
 export class App {
@@ -9,6 +10,9 @@ export class App {
   ea: EventAggregator;
   api: HttpClient;
 
+  gradient: CanvasGradient = null;
+  gradientHeight: number = null;
+  gradientWidth: number = null;
   temperature: string = 'Hetkeinfo ei ole kättesaadaval';
   temperature2: string = 'Seda pole ka hetkel olemas';
   temperatureData: any[];
@@ -86,16 +90,51 @@ export class App {
     let temperatures = this.temperatureData;
     temperatures = temperatures.slice(Math.max(temperatures.length - this.selectedPeriod.dataPointsCount, 0));
     temperatures = temperatures.filter((temp, i) => i % (this.selectedPeriod.filterNumber - 1) == 0);
-    const data1 = {
+    const data1: ChartJs.ChartDataSets = {
       label: 'Välistemperatuur',
       data: temperatures.map(e => e.temp1 / 10),
-      borderColor: 'rgba(255,99,132,1)',
+      borderColor: (context) => {
+        const chartArea = context.chart.chartArea;
+
+        if (!chartArea) {
+          return 'rgba(255,99,132,1)';
+        }
+        const chartWidth = chartArea.right - chartArea.left;
+        const chartHeight = chartArea.bottom - chartArea.top;
+
+        if (this.gradient === null || this.gradientWidth !== chartWidth || this.gradientHeight !== chartHeight) {
+          this.gradientWidth = chartWidth;
+          this.gradientHeight = chartHeight;
+
+          console.log(context)
+
+          let colorChangeOffset = 0.5;
+          const yScale: any = context.chart['scales']['y-axis-0'];
+          if (yScale.start * yScale.end < 0) {
+            const distance = yScale.end - yScale.start
+            colorChangeOffset = (distance - yScale.end) / distance
+          } else if (yScale.start >= 0) {
+            colorChangeOffset = 0
+          } else if (yScale.end <= 0) {
+            colorChangeOffset = 1
+          }
+        console.log(colorChangeOffset)
+
+          this.gradient = context.chart.ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+          this.gradient.addColorStop(1, 'rgba(255,99,132,1)',)
+          this.gradient.addColorStop(Math.min(colorChangeOffset + 0.05, 1), 'rgba(255,99,132,1)',)
+          this.gradient.addColorStop(Math.max(colorChangeOffset - 0.05, 0), 'rgb(70,118,255)',)
+          this.gradient.addColorStop(0, 'rgb(70,118,255)',)
+        }
+
+        return this.gradient;
+      },
       backgroundColor: 'rgba(255,135,157,1)',
       borderWidth: 3,
       pointRadius: 0,
       fill: false,
     };
-    const data2 = {
+    const data2: ChartJs.ChartDataSets = {
       label: 'Kasvuhoone',
       data: temperatures.map(e => e.temp2 / 10),
       borderColor: 'rgb(0,215,144)',
